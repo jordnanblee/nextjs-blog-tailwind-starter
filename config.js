7df333343c247d256f947d717843b7f049222ad5
jordnanblee <jordnanblee2@proton.me>
2025-07-18 16:05:02 +0300

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
-export const AUTHOR_NAME = 'Nuno Marques'
-export const SITE_URL = 'https://design-code.tips'
-export const SITE_NAME = 'design-code.tips'
+export const AUTHOR_NAME = 'Neil Blee'
+export const SITE_URL = 'https://neilgames.netlify.app'
+export const SITE_NAME = 'neilgames.netlify.app'
 export const SHOW_PER_PAGE = 10
